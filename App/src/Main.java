05892136441a9816c8ad1881a5af519ed3461523
adamgiertuga <adamgiertuga@gmail.com>
2020-05-11 20:55:30 +0200

    Liczenie pierwiastka

--- App/src/Main.java
+++ App/src/Main.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main (String[] args){
         System.out.println("Helo World!");
         System.out.println("Podaj liczbę, którą należy spierwiastkować: ");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        System.out.println(Math.sqrt(number));
+
     }
 }
